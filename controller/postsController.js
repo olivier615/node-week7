e830b9568fd5d1da2a2efc874cf83a7f5860dd0f
Otis Chen <variety928@gmail.com>
2022-06-05 04:04:40 +0800

    '修正留言圖片'

--- controller/postsController.js
+++ controller/postsController.js
@@ -13,7 +13,7 @@ exports.getAllPosts = async (req, res, next) => {
     select: 'name photo'
   }).populate({
     path: 'comments',
-    select: 'comment user'
+    select: 'comment user image'
   }).sort(timeSort)
   handelSuccess(res, allPosts)
 }
